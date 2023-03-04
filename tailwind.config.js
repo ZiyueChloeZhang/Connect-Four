559cda81e880dcc1db5ad264b5aebd45589508df
Chloe Zhang <chloezhang1030@gmail.com>
2023-03-04 13:11:48 -0800

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      black: "#000000",
+      "purple-dark": "#5c2dd5",
+      purple: "#7945ff",
+      pink: "#FD6687",
+      yellow: "#FFCE67",
+      white: "#FFFFFF",
+    },
+    fontSize: {
+      lg: ["56px", { lineHeight: "71px" }],
+      md: ["24px", { lineHeight: "31px" }],
+      sm: ["20px", { lineHeight: "26px" }],
+      xs: ["16px", { lineHeight: "21px" }],
+    },
+    boxShadow: {
+      DEFAULT: "0 10px 0",
+    },
+    borderWidth: {
+      DEFAULT: '3px'
+    },
     extend: {},
   },
   plugins: [],
